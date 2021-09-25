e28efecdb00e4c5b055c4dd1134aaa7d48317e81
rubakanaan <rubajkanaan@gmail.com>
2021-09-25 16:40:53 +0300

    create ract component and add every component

---
+++ partyistic/src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Inspiration from './components/Inspiration';
+import Parties from './components/Parties';
+import Profile from './components/Profile';
+import Services from './components/Services';
+import About from './components/About';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+
+export default function App() {
+  return (
+    <div style={{ height: '100%' }}>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/Inspiration" exact>
+            <Inspiration />
+          </Route>
+          <Route path="/Services" exact>
+            <Services />
+
+          </Route>
+
+          <Route path="/Parties" exact>
+            <Parties />
+          </Route>
+          <Route path="/Profile" exact>
+            <Profile />
+          </Route>
+          <Route path="/About" exact>
+            <About />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </div>
+  );
+}
