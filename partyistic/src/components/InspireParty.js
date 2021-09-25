2bb186d2449241f55633aff06e0436de8608912a
laithfayizhussein <laithalsanory9919@gmail.com>
2021-09-26 02:35:21 +0300

    temp data to run it in the inspiratian page and render 3 img only

---
+++ partyistic/src/components/InspireParty.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel'
+import Card from 'react-bootstrap/Card'
+import Nav from 'react-bootstrap/Nav'
+import Inspiration from './Inspiration';
+
+
+export default function InspireParty(props){
+    
+     return (
+        <>
+{        
+}        </>
+      );
+    
+  }
