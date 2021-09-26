47ba107d045c12437487fa72111cbf4ef9192c8c
rubakanaan <rubajkanaan@gmail.com>
2021-09-26 17:42:23 +0300

    party page : fix radio button

--- partyistic/src/components/Parties.js
+++ partyistic/src/components/Parties.js
@@ -3,7 +3,7 @@ import Button from 'react-bootstrap/Button'
 import Modal from 'react-bootstrap/Modal'
 import { useState } from 'react';
 import Form from 'react-bootstrap/Form'
-
+import InputGroup from 'react-bootstrap/InputGroup'
 
 
 export default function Parties() {
@@ -15,7 +15,7 @@ export default function Parties() {
     <>
       <h1>Parties</h1>
       <h2>Invited Parties</h2>
-      <Button variant="outline-secondary" onClick={handleShow}>Share Your Party</Button>
+      <Button variant="outline-secondary" onClick={handleShow}>Create Your Party</Button>
       <h2>Explore Public Partie</h2>
       <h3>Wedding Parties</h3>
       <h3>Graduation Parties</h3>
@@ -49,14 +49,12 @@ export default function Parties() {
               />
             </Form.Group>
             <Form.Group className="mb-3" controlId="privacy">
-              Privacy:   
-            <Form.Check type="radio" inline label="Public"/>
-            <Form.Check type="radio"  inline label="Private" />
+              Privacy:
+              <Form.Check type="radio" inline label="Public" id="public" name="privacy" value="public" />
+              <Form.Check type="radio" inline label="Private" id="private" name="privacy" value="private" />
             </Form.Group>
-
-
             <Form.Group className="mb-3" controlId="location">
-            <Form.Control
+              <Form.Control
                 as="textarea"
                 placeholder="Location Link"
                 style={{ height: '100px' }}
@@ -64,8 +62,8 @@ export default function Parties() {
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="city">
-            <Form.Select label="City">
-            <option value="1">City</option>
+              <Form.Select label="City">
+                <option value="1">City</option>
                 <option value="1">One</option>
                 <option value="2">Two</option>
                 <option value="3">Three</option>
@@ -73,10 +71,10 @@ export default function Parties() {
               </Form.Select>
             </Form.Group>
             <Form.Group className="mb-3" controlId="date">
-            <Form.Control type="date"  />
+              <Form.Control type="date" />
             </Form.Group>
             <Form.Group className="mb-3" controlId="invite">
-             
+
             </Form.Group>
             <Form.Group className="mb-3" controlId="email">
               <Form.Control type="email" placeholder="Invite People" />
@@ -87,7 +85,7 @@ export default function Parties() {
           </Form>
         </Modal.Body>
         <Modal.Footer>
-         
+
         </Modal.Footer>
       </Modal>
 
