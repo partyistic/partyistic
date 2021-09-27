cc290a36993a9523e200298b9e6d5b286092af06
nuratabanjeh <nuratabanjeh96@gmail.com>
2021-09-27 22:50:29 +0300

    Profile Page sections

--- partyistic/src/components/Profile.js
+++ partyistic/src/components/Profile.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Row from 'react-bootstrap/Row'
-import { Col, Image, Container, Card } from 'react-bootstrap'
-
+import {  Card } from 'react-bootstrap'
+import updateServiceForm from "./updateServiceForm"
+import { useState } from 'react';
 export default function Profile()  {
-    
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
      return (
         <>
         <h1>Personal Profile</h1>
@@ -19,23 +22,23 @@ export default function Profile()  {
 
                   <div className="col">
 
-                    <a href="/Services/places"> <Card.Img variant=""  alt="First image" src="https://images.unsplash.com/photo-1605152276897-4f618f831968?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" />
-                    </a>
+                     <Card.Img variant="" href={handleShow} alt="First image" src="https://images.unsplash.com/photo-1605152276897-4f618f831968?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" />
+                    
                     </div>
 
                   <div className="col">
-                  <a href="/Services/places"> <Card.Img alt="Second image" src="https://images.unsplash.com/photo-1605152276897-4f618f831968?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" />
-                  </a>
+                   <Card.Img alt="Second image" href={handleShow} src="https://images.unsplash.com/photo-1605152276897-4f618f831968?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" />
+                  
                   </div>
 
                   <div className="col">
-                  <a href="/Services/places"> <Card.Img variant="" alt="Third image" src="https://images.unsplash.com/photo-1605152276897-4f618f831968?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" />
-                  </a>
+                   <Card.Img variant="" alt="Third image" href={handleShow} src="https://images.unsplash.com/photo-1605152276897-4f618f831968?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" />
+                  
                   </div>
 
                   <div className="col">
-                  <a href="/Services/places"> <Card.Img alt="Second image" src="https://images.unsplash.com/photo-1605152276897-4f618f831968?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" />
-                  </a>
+                   <Card.Img alt="Second image" href={handleShow} src="https://images.unsplash.com/photo-1605152276897-4f618f831968?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" />
+                  
 
                   </div>
 
@@ -43,6 +46,78 @@ export default function Profile()  {
                   <div className="col"></div>
 
                   </div>
+
+
+
+
+
+                  <h2>Your Favorites</h2>
+                 <div className="row row-cols-6">
+
+                <div className="col"></div>
+
+                  <div className="col">
+
+                     <Card.Img variant="" onClick={handleShow} alt="First image" src="https://images.unsplash.com/photo-1603481502506-275d81ffcdef?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=735&q=80" />
+                    
+                    </div>
+
+                  <div className="col">
+                   <Card.Img alt="Second image" onClick={handleShow} src="https://images.unsplash.com/photo-1603481502506-275d81ffcdef?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=735&q=80" />
+                  
+                  </div>
+
+                  <div className="col">
+                   <Card.Img variant="" alt="Third image" onClick={handleShow} src="https://images.unsplash.com/photo-1603481502506-275d81ffcdef?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=735&q=80" />
+                  
+                  </div>
+
+                  <div className="col">
+                   <Card.Img alt="Second image" onClick={handleShow} src="https://images.unsplash.com/photo-1603481502506-275d81ffcdef?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=735&q=80" />
+                  
+
+                  </div>
+
+
+                  <div className="col"></div>
+
+                  </div>
+
+                  <h2>Your Parties</h2>
+                 <div className="row row-cols-6">
+
+                <div className="col"></div>
+
+                  <div className="col">
+
+                     <Card.Img variant="" onClick={handleShow} alt="First image" src="https://images.unsplash.com/photo-1509666537727-9154b6962292?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" />
+                    
+                    </div>
+
+                  <div className="col">
+                   <Card.Img alt="Second image" onClick={handleShow} src="https://images.unsplash.com/photo-1509666537727-9154b6962292?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" />
+                  
+                  </div>
+
+                  <div className="col">
+                   <Card.Img variant="" alt="Third image" onClick={handleShow} src="https://images.unsplash.com/photo-1509666537727-9154b6962292?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" />
+                  
+                  </div>
+
+                  <div className="col">
+                   <Card.Img alt="Second image" onClick={handleShow} src="https://images.unsplash.com/photo-1509666537727-9154b6962292?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" />
+                  
+
+                  </div>
+
+
+                  <div className="col"></div>
+
+                  </div>
+
+
+
+                  <updateServiceForm show={show} handleClose={handleClose} />
         </>
       );
     
