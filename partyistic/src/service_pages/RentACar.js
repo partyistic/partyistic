d897d3cd8772e3db45a228592c8ad19ba01591a6
FIRAS AHMAD HASAN <x.firashasan@gmail.com>
2021-09-28 23:17:14 +0300

    WIP: services-page filter inputs

--- partyistic/src/service_pages/RentACar.js
+++ partyistic/src/service_pages/RentACar.js
@@ -1,15 +1,47 @@
 /** @format */
-
-import React from 'react';
-import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
-import Carousel from 'react-bootstrap/Carousel';
-import { Card, Form } from 'react-bootstrap';
-import useCars from '../hook/useServicesCars';
-import { useState } from 'react';
+import React from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import Carousel from "react-bootstrap/Carousel";
+import { Card, Form } from "react-bootstrap";
+import useCars from "../hook/useServicesCars";
+import { useState } from "react";
 
 export default function RentACar() {
-  const cars = useCars().resources;
+  const originalCars = useCars().resources;
+
+  const [cars, setCars] = useState(originalCars);
+  const App = () => {
+    setCars(useCars().resources);
+  };
+
+  const [price, setPrice] = useState(2000000000);
+  const getPrice = (event) => {
+    setPrice(event.target.value);
+  };
+
+  let city = document.getElementById("city");
+  const [selectedCity, setCity] = useState("City")
+  const getCityValue = () => {
+    // let selectedCity = city.options[city.selectedIndex];
+    setCity(city.options[city.selectedIndex].value)
+  };
+  console.log(selectedCity);
+
+  function filtering() {
+    let lister = [];
+
+    originalCars.map((item) => {
+      if ( ((item.price) <= price && (item.city == selectedCity || selectedCity == "City")) ) {
+        lister.push(item);
+        console.log(lister, "trippeieirer");
+        setCars(lister);
+      } else {
+        
+        console.log(lister,"trying to sdo it")
+      }
+    });
+  }
 
   const [showCar, setShowCar] = useState(false);
   const [car, setCar] = useState(false);
@@ -28,7 +60,7 @@ export default function RentACar() {
               <Modal.Title>{car.name}</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-              <Card style={{ width: '18rem' }}>
+              <Card style={{ width: "18rem" }}>
                 <Card.Body>
                   {car.description}
                   {car.images && (
@@ -73,16 +105,31 @@ export default function RentACar() {
   };
   return (
     <>
-      <div className='row row-cols-4'>
+      <div className='row row-cols-6'>
         <div className='col '></div>
 
         <div className='col'>
-          <Form.Select className='col ' aria-label='Default select example'>
-            <option>City</option>
-            <option value='1'>Amman</option>
-            <option value='2'>Zarqa</option>
-            <option value='3'>Irbid</option>
-            <option value='3'>Jarash</option>
+          <Form.Select
+          id='city'
+            className='col '
+            onChange={getCityValue}
+            aria-label='Default select example'>
+            <option value="City" selected>City</option>
+            <option value='Amman'>Amman</option>
+            <option value='Zarqa'>Zarqa</option>
+            <option value='Irbid'>Irbid</option>
+            <option value='Al-Mafraq'>Al-Mafraq</option>
+            <option value='Jarash'>Jarash</option>
+            <option value='Ajloun'>Ajloun</option>
+            <option value='As-Salt'>As-Salt</option>
+            <option value='Madaba'>Madaba</option>
+            <option value='Karak'>Karak</option>
+            <option value='Tafilah'>Tafilah</option>
+            <option value='Maan'>Maan</option>
+            <option value='Aqaba'>Aqaba</option>
+
+
+            
           </Form.Select>
         </div>
 
@@ -95,9 +142,23 @@ export default function RentACar() {
             <option value='3'>April</option>
           </Form.Select>
         </div>
-
         <div className='col'>
-          <input type='text' placeholder='Maximum Price'></input>
+          <Button variant='primary' onClick={filtering}>
+            {" "}
+            ALL TRIPS
+          </Button>
+        </div>
+        <div className='col'>
+          <input
+            type='text'
+            onChange={getPrice}
+            placeholder='Maximum Price'></input>
+        </div>
+        <div className='col'>
+          <Button variant='primary' onClick={filtering}>
+            {" "}
+            SEARCH
+          </Button>
         </div>
       </div>
 
@@ -107,7 +168,7 @@ export default function RentACar() {
           cars.map((item) => (
             <div className='col'>
               <Card
-                style={{ width: '18rem' }}
+                style={{ width: "18rem" }}
                 onClick={() => {
                   handleShow(item);
                 }}>
