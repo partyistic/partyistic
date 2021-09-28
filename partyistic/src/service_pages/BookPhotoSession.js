54c7e3eeecf6491239e8536c8497893117fe1d6e
laithfayizhussein <laithalsanory9919@gmail.com>
2021-09-29 02:07:51 +0300

    add filter for two pages one for session page and fathion

--- partyistic/src/service_pages/BookPhotoSession.js
+++ partyistic/src/service_pages/BookPhotoSession.js
@@ -1,15 +1,60 @@
 /** @format */
 
-import React from 'react';
-import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
-import Carousel from 'react-bootstrap/Carousel';
-import { Card, Form } from 'react-bootstrap';
-import usePhotosessions from '../hook/useServicesPhotosessions';
-import { useState } from 'react';
+import React from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import Carousel from "react-bootstrap/Carousel";
+import { Card, Form } from "react-bootstrap";
+import usePhotosessions from "../hook/useServicesPhotosessions";
+import { useState } from "react";
 
 export default function BookPhotoSession() {
-  const photosessions = usePhotosessions().resources;
+  const originalPhotosessions = usePhotosessions().resources;
+
+  const [photosessions, setPhotosessions] = useState(originalPhotosessions);
+  const App = () => {
+    setPhotosession(usePhotosessions().resources);
+  };
+
+  const [price, setPrice] = useState(200000000000000);
+  const getPrice = (event) => {
+    setPrice(event.target.value);
+  };
+
+  let date = document.getElementById("date");
+  const [selectedDate, setDate] = useState("Available Date");
+  const getDateValue = () => {
+    // let selectedCity = city.options[city.selectedIndex];
+    setDate(date.options[date.selectedIndex].value);
+  };
+  console.log(selectedDate);
+
+  let city = document.getElementById("city");
+  const [selectedCity, setCity] = useState("City");
+  const getCityValue = () => {
+    setCity(city.options[city.selectedIndex].value);
+  };
+  console.log(selectedCity);
+
+  function filtering() {
+    let lister = [];
+
+    originalPhotosessions.map((item) => {
+      if (
+        (item.price <= price || item.price == "") &&
+        (item.city == selectedCity || selectedCity == "City") &&
+        (item.booked_dates == null ||
+          item.booked_dates.dates[0] == selectedDate ||
+          item.booked_dates.dates[1] == selectedDate ||
+          selectedDate == "Available Date")
+      ) {
+        lister.push(item);
+        setPhotosessions(lister);
+      } else {
+        setPhotosessions([]);
+      }
+    });
+  }
 
   const [showPhotosession, setShowPhotosession] = useState(false);
   const [photosession, setPhotosession] = useState(false);
@@ -28,7 +73,7 @@ export default function BookPhotoSession() {
               <Modal.Title>{photosession.name}</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-              <Card style={{ width: '18rem' }}>
+              <Card style={{ width: "18rem" }}>
                 <Card.Body>
                   {photosession.description}
                   {photosession.images && (
@@ -69,33 +114,63 @@ export default function BookPhotoSession() {
       return <> </>;
     }
   };
+
   return (
     <>
-      <div className='row row-cols-4'>
+      <div className='row row-cols-6'>
         <div className='col '></div>
 
         <div className='col'>
-          <Form.Select className='col ' aria-label='Default select example'>
-            <option>City</option>
-            <option value='1'>Amman</option>
-            <option value='2'>Zarqa</option>
-            <option value='3'>Irbid</option>
-            <option value='3'>Jarash</option>
+          <Form.Select
+            id='city'
+            className='col '
+            onChange={getCityValue}
+            aria-label='Default select example'>
+            <option value='City' selected>
+              City
+            </option>
+            <option value='Amman'>Amman</option>
+            <option value='Zarqa'>Zarqa</option>
+            <option value='Irbid'>Irbid</option>
+            <option value='Al-Mafraq'>Al-Mafraq</option>
+            <option value='Jarash'>Jarash</option>
+            <option value='Ajloun'>Ajloun</option>
+            <option value='As-Salt'>As-Salt</option>
+            <option value='Madaba'>Madaba</option>
+            <option value='Karak'>Karak</option>
+            <option value='Tafilah'>Tafilah</option>
+            <option value='Maan'>Maan</option>
+            <option value='Aqaba'>Aqaba</option>
           </Form.Select>
         </div>
 
         <div className='col'>
-          <Form.Select className='col ' aria-label='Default select example'>
-            <option>Available Month</option>
-            <option value='1'>January</option>
-            <option value='2'>February</option>
-            <option value='3'>March</option>
-            <option value='3'>April</option>
+          <Form.Select
+            className='col'
+            id='date'
+            onChange={getDateValue}
+            aria-label='Default select example'>
+            <option value='Available Date'>Available Date</option>
+            <option value='2021/19/3'>2021/19/3</option>
+            <option value='2020/10/1'>2020/10/1</option>
+            <option value='2013/31/1'>2013/31/1</option>
+            <option value='32011/24/4'>2011/24/4</option>
           </Form.Select>
         </div>
-
+        <div className='col'>
+          <Button variant='primary' onClick={filtering}>
+            {" "}
+            ALL PHOTOSESSIONS
+          </Button>
+        </div>
         <div className='col'>
           <input type='text' placeholder='Maximum Price'></input>
+        </div>
+        <div className='col'>
+          <Button variant='primary' onClick={filtering}>
+            {" "}
+            SEARCH
+          </Button>
         </div>
       </div>
 
@@ -106,7 +181,7 @@ export default function BookPhotoSession() {
           photosessions.map((item) => (
             <div className='col'>
               <Card
-                style={{ width: '18rem' }}
+                style={{ width: "18rem" }}
                 onClick={() => {
                   handleShow(item);
                 }}>
